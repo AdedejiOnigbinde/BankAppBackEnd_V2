859002e7fde71700e045e882e183c50ae44df918
Adedeji Onigbinde <adedejionigbinde@gmail.com>
2023-03-05 18:29:46 -0500

    Completion of service Layer

---
+++ src/main/java/com/base/BaseDependencies/Controller/ClientController.java
@@ -0,0 +1,69 @@
+package com.base.BaseDependencies.Controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.base.BaseDependencies.Dtos.LoginClientDto;
+import com.base.BaseDependencies.Dtos.RegClientDto;
+import com.base.BaseDependencies.Models.Client;
+import com.base.BaseDependencies.Service.ClientService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerClient(@RequestBody RegClientDto regClientDto) {
+        try {
+            String hasRegistered = clientService.createClient(regClientDto);
+            return new ResponseEntity<>(hasRegistered, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginClient(@RequestBody LoginClientDto loginClientDto){
+        try {
+            String verifiedClient = clientService.verifyClient(loginClientDto);
+            return new ResponseEntity<>(verifiedClient, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @GetMapping("/allclients")
+    public ResponseEntity<?> getAllClient(){
+        try {
+            List<Client> clientList = clientService.getAllClients();
+            return new ResponseEntity<>(clientList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @DeleteMapping("/removeclient")
+    public ResponseEntity<?> deleteClient(@RequestHeader("userToekn") String userToken){
+        try {
+            clientService.deleteClient(userToken);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+}
--- src/main/java/com/base/BaseDependencies/Models/Transaction.java
+++ src/main/java/com/base/BaseDependencies/Models/Transaction.java
@@ -3,7 +3,6 @@ package com.base.BaseDependencies.Models;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,17 +24,17 @@ import lombok.NoArgsConstructor;
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(updatable = false, nullable = false, unique = true)
+    @Column(name="trans_id",updatable = false, nullable = false, unique = true)
     private int transId;
 
     private String transType;
     
     private double transAmount;
     
-    @JoinColumn(name = "accountNumber")
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "account_number")
     @JsonManagedReference
-    private Account fromAcct;
+    private Account fromAccount;
     
     private long toAcct;
 
